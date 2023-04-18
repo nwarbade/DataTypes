0ef33fd72e3005f351dfe3f9f4da42e17ab510f9
nwarbade <122026632+nwarbade@users.noreply.github.com>
2023-04-18 19:02:08 +0530

    Initial commit

---
+++ src/com/dataTypes/NonPremitiveDataType.java
@@ -0,0 +1,12 @@
+package com.dataTypes;
+
+public class NonPremitiveDataType {
+	
+	public static void main(String[] args) {
+		
+		String a = "my name is:-";
+		
+		System.out.println(a +"nana");
+	}
+
+}
---
+++ src/com/dataTypes/PrimitiveDataType.java
@@ -0,0 +1,28 @@
+package com.dataTypes;
+
+public class PrimitiveDataType {
+	
+	public static void main(String[] args) {
+		
+		// Data type   Variable  = Value;
+		
+		boolean b = false;
+		byte e = -123;
+		short f = 3213;
+		int i = 213454;
+		long l = 1234567256;
+		float g = 32.34f;
+		double d = 789.99d;
+		char k = 'k';
+		
+		System.out.println(b);
+		System.out.println(f);
+		System.out.println(e);
+		System.out.println(i);
+		System.out.println(l);
+		System.out.println(g);
+		System.out.println(d);
+		System.out.println(k);
+	}
+
+}
