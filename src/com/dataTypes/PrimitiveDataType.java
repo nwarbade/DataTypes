aa43ff4d9dd9ee2a7966451f5e4c36981ce1d964
nwarbade <122026632+nwarbade@users.noreply.github.com>
2023-04-18 22:30:06 +0530

    Update PrimitiveDataType.java
    comment added

--- src/com/dataTypes/PrimitiveDataType.java
+++ src/com/dataTypes/PrimitiveDataType.java
@@ -7,7 +7,7 @@ public class PrimitiveDataType {
 		// Data type   Variable  = Value;
 		
 		boolean b = false;  // the boolean data type is used to represent a value that can either be true or false
-		byte e = -123;
+		byte e = -123;  //byte data type is a primitive data type that represents an 8-bit signed two's complement integer. It has a minimum value of -128 and a maximum value of 127 (inclusive).
 		short f = 3213;
 		int i = 213454;
 		long l = 1234567256;
