1a542eb2d50ce94abbd380a98c2afcddd134feee
nwarbade <122026632+nwarbade@users.noreply.github.com>
2023-04-19 19:40:37 +0530

    Update NonPremitiveDataType.java
    Comment added

--- src/com/dataTypes/NonPremitiveDataType.java
+++ src/com/dataTypes/NonPremitiveDataType.java
@@ -10,3 +10,8 @@ public class NonPremitiveDataType {
 	}
 
 }
+
+
+//the String data type represents a sequence of characters. It is a built-in class in the Java language 
+//and is used extensively in Java programming. 
+//Strings are immutable, which means that once a string is created, its contents cannot be changed
