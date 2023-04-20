33f03e60a28df3f6047c16767fb698781eb03f19
nwarbade <122026632+nwarbade@users.noreply.github.com>
2023-04-20 18:07:35 +0530

    Update NonPremitiveDataType.java
    comments added

--- src/com/dataTypes/NonPremitiveDataType.java
+++ src/com/dataTypes/NonPremitiveDataType.java
@@ -15,3 +15,21 @@ public class NonPremitiveDataType {
 //the String data type represents a sequence of characters. It is a built-in class in the Java language 
 //and is used extensively in Java programming. 
 //Strings are immutable, which means that once a string is created, its contents cannot be changed
+
+/*In Java, there are several non-primitive data types, also known as reference types or object types, which are used to store complex data and objects. These data types include:
+
+Classes: Classes are used to define objects and their properties and methods. They are used to create objects of a particular type.
+
+Interfaces: Interfaces are similar to classes but they define a set of methods that a class must implement. They are used to create a common interface for classes that share similar functionality.
+
+Arrays: Arrays are used to store a collection of data of the same type. They are created with a fixed size and can be accessed using an index.
+
+Strings: Strings are used to store text data. They are objects of the String class and can be manipulated using various methods.
+
+Enumerations: Enumerations are used to define a fixed set of values. They are created using the enum keyword and can be used to create a type-safe collection of values.
+
+Collections: Collections are used to store a collection of objects. They are part of the Java Collections Framework and include classes such as List, Set, and Map.
+
+Maps: Maps are used to store key-value pairs. They are part of the Java Collections Framework and are implemented using the Map interface.
+
+These non-primitive data types provide a way to represent and manipulate complex data structures and objects in Java.  */
